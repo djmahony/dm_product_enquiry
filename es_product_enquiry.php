ae01fa519005e417b07e8d89f0b24d92b0b6dd23
djmahony <danmahony85@gmail.com>
2023-01-12 23:12:20 +0000

    Added checkbox to product options (general tab)

--- es_product_enquiry.php
+++ es_product_enquiry.php
@@ -1,5 +1,32 @@
 <?php
+/*
+Plugin Name: eSterling Product Enquiry
+Plugin URI:  http://www.esterling.co.uk
+Description: Set up a new "enquiry" post type and allow customers to enquire about products
+Version:     1.0
+Author:      eSterling (Dan Mahony)
+Author URI:  http://www.esterling.co.uk
+*/
 
 class es_product_enquiry {
 
+    public function __construct() {
+        add_action('woocommerce_product_options_general_product_data', [$this, 'es_product_enquiry_checkbox']);
+    }
+
+    public function es_product_enquiry_checkbox() {
+        echo '<div class="options_group">';
+        woocommerce_wp_checkbox(
+            array(
+                'id'      => 'enquiry_product',
+                'value'   => get_post_meta( get_the_ID(), 'enquiry_product', true ),
+                'label'   => 'Enquiry Product?',
+                'desc_tip' => true,
+                'description' => 'Add to basket will be replaced with an enquiry button if ticked',
+            )
+        );
+        echo '</div>';
+    }
 }
+
+$es_product_enquiry = new es_product_enquiry();
