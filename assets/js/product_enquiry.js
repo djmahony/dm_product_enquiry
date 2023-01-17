9cf1b6b1fd5496095fb85a06a53ce0c576bb77c8
djmahony <danmahony85@gmail.com>
2023-01-17 12:32:08 +0000

    js for enquiry form submit

--- assets/js/product_enquiry.js
+++ assets/js/product_enquiry.js
@@ -1,20 +1,53 @@
 (function () {
 
+    const enquiry_form = document.querySelector('.es_enquiry_form');
+
     document.addEventListener('click', (event) => {
 
-        console.log(event);
-        if (!event.target.matches('#es_product_enquiry_btn, .es_enquiry_form_overlay')) return;
+        if (!event.target.matches('#es_product_enquiry_btn, .es_enquiry_form_overlay, .es_enquiry_form__close--icon')) return;
 
         const addClass = (el, className) => {
             el.classList.toggle(className);
         }
 
         const overlay = document.querySelector('.es_enquiry_form_overlay');
-        const enquiry_form = document.querySelector('.es_enquiry_form');
 
         addClass(overlay, 'active');
         addClass(enquiry_form, 'active');
-    })
+    });
 
+    enquiry_form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
+        const format_data = (form) => {
+            let data_obj = {};
+            const form_data = new FormData(form);
+            for(let key of form_data.keys()) {
+                data_obj[key] = form_data.get(key);
+            }
+
+            return data_obj;
+        }
+
+        let submit_data = format_data(event.target);
+        submit_data.action = 'es_product_enquiry_submit';
+        submit_data.nonce = ajax_object.nonce;
+
+        jQuery.ajax({
+            type: 'POST',
+            cache: false,
+            url: ajax_object.ajax_url,
+            data: submit_data,
+            success: function(data) {
+                let html = '<div class="es_enquiry_form__close"> ' +
+                    '<div class="es_enquiry_form__close--icon"></div> ' +
+                    '</div>' +
+                    '<div class="success_message">Thank you for your enquiry, someone will be in touch soon</div>';
+                jQuery('.es_enquiry_form').html(html);
+            },
+            error: function(errorThrown){
+                console.log(errorThrown);
+            }
+        });
+    });
 })();
