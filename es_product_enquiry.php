94bbfd32cdae47864b55971db1bc2551e3f002f8
djmahony <danmahony85@gmail.com>
2023-01-17 12:32:24 +0000

    Added action to handle ajax request

--- es_product_enquiry.php
+++ es_product_enquiry.php
@@ -26,6 +26,10 @@ class es_product_enquiry {
         $post_type = new es_enquiry_post_type();
 
         add_action('wp', [$this, 'woocommerce_actions']);
+
+        // Add Ajax actions for form submission
+        add_action( 'wp_ajax_es_product_enquiry_submit', array($this, 'add_enquiry') );
+        add_action( 'wp_ajax_nopriv_es_product_enquiry_submit', array($this, 'add_enquiry') );
     }
 
     /**
@@ -63,6 +67,7 @@ class es_product_enquiry {
 
         wp_register_script('es_product_enquiry_js', plugin_dir_url( __FILE__ ) . 'assets/js/product_enquiry.js', '', '', true);
         wp_enqueue_script('es_product_enquiry_js');
+        wp_localize_script('es_product_enquiry_js', 'ajax_object', ['ajax_url' => admin_url('admin-ajax.php'), 'nonce' => wp_create_nonce('ajax-nonce')]);
         wp_enqueue_style('es_product_enquiry_css', plugin_dir_url( __FILE__ ) . 'assets/css/style.css');
     }
 
@@ -80,8 +85,8 @@ class es_product_enquiry {
      * @return void
      */
     public function render_enquiry_form() {
-
-        $this->_load_template('enquiry_form.php');
+        global $product;
+        $this->_load_template('enquiry_form.php', ['id' => $product->get_id(), 'name' => $product->get_title()]);
     }
 
     /**
@@ -99,7 +104,42 @@ class es_product_enquiry {
             // Otherwise load it from the template directory in the plugin
             load_template(plugin_dir_path( __FILE__ ) . '/templates/' . $template, true, $args);
         }
+    }
 
+    public function add_enquiry() {
+
+
+        try {
+            $product = wc_get_product($_POST['product_id']);
+
+            $title = $_POST['name'] . ' ' . ' enquiry for ' . $product->get_name();
+            $post_args = [
+                'post_title' => $title,
+                'post_content' => $_POST['enquiry'],
+                'post_type' => 'product_enquiry',
+                'meta_input' => [
+                    '_enquiry_product_id' => $_POST['product_id'],
+                    '_enquiry_email' => $_POST['email']
+                ]
+            ];
+
+            wp_insert_post($post_args);
+
+            $to = 'daniel@esterilng.co.uk';
+            $subject = $title;
+            $headers = array('Content-Type: text/html; charset=UTF-8');
+            $email_body = "<p>You have received an enquiry from " . $_POST['email'] . "</p>
+                <p>Name: " . $_POST['name'] . "</p>
+                <p>Email: " . $_POST['email'] . "</p>
+                <p>Product: " . $product->get_id() . " - " . $product->get_name() . "</p>
+                <p>Enquiry: " . $_POST['enquiry'] . "</p>";
+
+            wp_mail($to, $subject, $email_body, $headers);
+
+        } catch(Exception $e) {
+
+        }
+        die;
     }
 
 }
